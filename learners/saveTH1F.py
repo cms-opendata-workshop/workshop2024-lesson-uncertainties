91b5dcf1aa0b738a5d59af818729e237716e9520
Julie Hogan <jmhogan806@gmail.com>
2024-07-31 07:45:46 -0500

    updates to Matt's format

--- learners/saveTH1F.py
+++ learners/saveTH1F.py
@@ -1,23 +1,31 @@
 from ROOT import TH1F, TFile
 import pickle
+import numpy as np
 
 f = open('hists_for_ROOT.p', 'rb')
 hists = pickle.load(f)
 f.close()
 
-xsec = {'signal':1.0,
-        'tt_semilep':831.76*0.438,
-        'tt_had':831.76*0.457,
-        'tt_lep':831.76*0.105,
-        'wjets':61526.7}
+# Goal is to write:
+# mtt__data_obs  (can't do right now)
+# mtt__signal
+# mtt__signal__puUp
+# mtt__signal__puDown
+# mtt__tt_semilep
+# mtt__tt_semilep__puUp....
+
+xsec = {'signal_M2000':1.0,
+        'ttsemilep':831.76*0.438,
+        'tthadronic':831.76*0.457,
+        'ttleptonic':831.76*0.105,
+        'Wjets':61526.7}
 
 roothists = {}
 for sample in hists.keys():
     print(sample)
 
     mtt = hists[sample]['mtt']
-
-    if sample == "data":
+    if sample == "collision":
         roothists[sample] = TH1F("mtt__data_obs",";m_{t#bar{t}} (GeV);events",50,0,3000)
         roothists[sample].FillN(len(mtt), mtt, np.ones(len(mtt)))
     else:
@@ -48,4 +56,7 @@ for ihist in roothists:
 
 output.Close()
 
-
+    
+#x = np.random.normal(0, 1, 1000)
+#hist = ROOT.TH1D("hist", "hist", 50, -3, 3)
+#hist.FillN(x.size, x, np.ones(x.size))
